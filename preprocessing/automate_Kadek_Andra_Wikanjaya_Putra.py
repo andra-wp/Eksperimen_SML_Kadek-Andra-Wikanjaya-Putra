8bf74610f18b6e0e33bc32def0a724cdec505424
Andra Wikanjaya <andrawpt@gmail.com>
2025-12-22 01:44:50 +0800

    Update automate_Kadek_Andra_Wikanjaya_Putra.py

--- preprocessing/automate_Kadek_Andra_Wikanjaya_Putra.py
+++ preprocessing/automate_Kadek_Andra_Wikanjaya_Putra.py
@@ -1,16 +1,36 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from pathlib import Path
 
-def preprocessing(df, output_path="diabetes_preprocessing.csv"):
+def preprocessing(df, target_col, output_path):
+    X = df.drop(columns=[target_col])
+    y = df[target_col].astype(int)
+
     scaler = StandardScaler()
-    df_scaled = scaler.fit_transform(df)
+    X_scaled = scaler.fit_transform(X)
 
-    df_final = pd.DataFrame(
-        df_scaled,
-        columns=df.columns,
+    X_final = pd.DataFrame(
+        X_scaled,
+        columns=X.columns,
         index=df.index
     )
 
+    df_final = pd.concat([X_final, y], axis=1)
     df_final.to_csv(output_path, index=False)
 
     return df_final
+
+
+if __name__ == "__main__":
+    BASE_DIR = Path(__file__).resolve().parent
+    DATA_PATH = BASE_DIR.parent / "diabetes_raw" / "diabetes.csv"
+    OUTPUT_PATH = BASE_DIR.parent / "diabetes_preprocessing.csv"
+
+    df = pd.read_csv(DATA_PATH)
+
+    preprocessing(
+        df,
+        target_col="Outcome",
+        output_path=OUTPUT_PATH
+    )
+
